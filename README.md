2e2abc7121e6025c78715ac5f12a41955b972e3c
furkan <furkanipek.93@gmail.com>
2016-11-08 23:15:32 +0300

    Updated README and added a simple login form to front-end

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # arge
+
+Front-end and back-end are deployed separately.
+
+After pulling the repo:
+
+cd front-end
+npm install
+npm start
+
+And front end will start on localhost:3000
+
+cd back-end
+npm install
+node server.js
+
+Aaand back end will be running on localhost:2999
+
+Of course mongodb and express should be available for back-end to work.
--- front-end/app/app.component.ts
+++ front-end/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   variable = "This is a variable"
+  // TODO *Str variables should be reloaded with their new values when user changes the language
+  signUpStr = "Signup"
+  userNameStr = "Username"
+  passwordStr = "Password"
+  authStr = "Login"
+  emailStr = "E-mail"
+  auth = function() {
+    console.log("Authorized!");
+  }
 }

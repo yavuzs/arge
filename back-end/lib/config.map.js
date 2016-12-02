95a1c9fafbaaa74c965363f7f8f4e8871ae9b0ad
Erol Guzoğlu <erolguzoglu@gmail.com>
2016-12-02 21:50:36 +0300

    added port var to config

--- back-end/lib/config.map.js
+++ back-end/lib/config.map.js
@@ -1,4 +1,5 @@
 const config = {
+    port = 3000,
     mongoUrl: 'mongodb://localhost:27017/arge',
     userSchema: 'users',
 }
--- back-end/server.js
+++ back-end/server.js
@@ -1,20 +1,22 @@
+var config = require('./lib/config.map');
+
 const express = require('express');
 const mongoClient = require('mongodb').MongoClient;
 const app = express();
 
-var config = require('./lib/config.map');
-
 // precedence matters!
 app.use(require('./lib/user/route'));
 
-mongoClient.connect(config.mongoUrl, function name(err, database) {
+mongoClient.connect(config.mongoUrl, function (err, database) {
     if (err) {
         console.error(err)
     }
     else { // do not bootstrap back-end if db is not available
-        app.listen(2999, function name() {
-            console.log("Listening on port 2999");
+        app.listen(config.port, function name() {
+            console.log("Listening on port 3000");
         });
+	//TO-DO
+	//Please add comment here :)
         //database.dropDatabase();
         database.close();
     }   
